e80961c54b8dc3d99200f427f2fa34547d032672
Ayush Gupta <ayush4002gupta@gmail.com>
2025-01-17 01:19:07 +0530

    database and prisma initialized

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -13,6 +14,58 @@ const handler = NextAuth({
     strategy: "jwt",
   },
   debug: true, // Enable debug mode for detailed error logs
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const email = user.email;
+      const image = user.image;
+      const name = user.name;
+
+      try {
+        console.log("User:", user);
+        if(!email){
+          return false;
+        }
+        const userExist = await prisma.user.findUnique({
+          where: {
+            email :email || "",
+          },
+          
+        })
+        if(!userExist){
+          await prisma.user.create({
+            data: {
+              email : email,
+              name : name,
+              apiKey : "123456",
+            },
+          });
+        }
+        // Check if user already exists
+        // const existingUser = await prisma.user.findUnique({
+        //   where: { email },
+        // });
+
+        // if (!existingUser) {
+        //   // If not, create a new user
+        //   await prisma.user.create({
+        //     data: {
+        //       email,
+        //       image,
+        //     },
+        //   });
+        // }
+      } catch (error) {
+        console.error("Error inserting user into the database:", error);
+        return false; // Return false to reject the sign-in
+      }
+
+      return true; // Return true to continue the sign-in process
+    },
+    async redirect({ url, baseUrl }) {
+      // Redirect user to the dashboard after sign-in
+      return `${baseUrl}/dashboard`;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
